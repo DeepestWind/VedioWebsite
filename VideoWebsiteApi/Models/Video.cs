2d18078daa612d365537ab60677f973a84ed8014
Dawn_Chen <1041031300@qq.com>
2024-02-21 15:24:41 +0800

    建立video模型并测试Initial commit

---
+++ VideoWebsiteApi/Models/Video.cs
@@ -0,0 +1,14 @@
+﻿namespace VideoWebsiteApi.Models
+{
+    public class Video
+    {
+        public int VideoId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostedOn { get; set; }//视频上传的日期和时间。
+        public TimeSpan Duration { get; set; }//视频的持续时间。
+        public string Poster { get; set; }//视频的发布者。
+        public string VideoFilePath { get; set; }//视频文件的路径。
+        public string ThumbnailsPath { get; set; }//视频的缩略图路径。
+    }
+}
