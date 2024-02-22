56b959e58fde0b9396e2b0afe7bfb57e96a94587
Dawn_Chen <1041031300@qq.com>
2024-02-22 19:27:17 +0800

    add video-card

--- WebsiteUI/src/app/home/home.component.ts
+++ WebsiteUI/src/app/home/home.component.ts
@@ -4,10 +4,11 @@ import { HeaderModule } from '../header/header.module';
 import { HttpClientModule } from '@angular/common/http'; 
 import { VideoService } from '../services/video.service';
 import { Video } from '../models/video.model';
+import { VideoCardComponent } from '../video-card/video-card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,HttpClientModule,HeaderModule],
+  imports: [CommonModule,HttpClientModule,HeaderModule,VideoCardComponent],
   providers: [VideoService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
--- WebsiteUI/src/app/video-card/video-card.component.ts
+++ WebsiteUI/src/app/video-card/video-card.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Video } from '../models/video.model';
 @Component({
   selector: 'app-video-card',
   standalone: true,
@@ -8,9 +8,15 @@ import { Input } from '@angular/core';
   styleUrl: './video-card.component.css'
 })
 export class VideoCardComponent {
-  @Input() video: any; // `any`可以替换为一个更加具体的视频类型接口
-  navigateToVideoDetail(video: any) {
-    // 这里替换为你的路由逻辑，可能会用到 Angular 的 Router
-    // 例如: this.router.navigate(['/video', video.id]);
+  @Input() video: Video ={
+    videoId: 0,
+    title: 'unfinded',
+    description: 'unfinded',
+    postedOn: new Date(),
+    duration: 'unfinded',
+    poster: 'unfinded',
+    videoFilePath: 'unfinded',
+    thumbnailsPath: 'unfinded'
+
   }
 }
