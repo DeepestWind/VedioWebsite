cc160003786b3159f03c9c23df803fb25b41e768
Dawn_Chen <1041031300@qq.com>
2024-03-12 15:20:43 +0800

    get data from database

--- VideoWebsiteApi/Controllers/VideoController.cs
+++ VideoWebsiteApi/Controllers/VideoController.cs
@@ -3,70 +3,55 @@ using VideoWebsiteApi.Models;
 using Microsoft.Extensions.Configuration;
 using VideoWebsiteApi.Services;
 using VideoWebsiteApi.Interfaces;
+using VideoWebsiteApi.Data;
 namespace VideoWebsiteApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class VideoController : ControllerBase
     {
-        private List<Video> _videos;
         private readonly IFileUrlService _fileUrlService;
-        public VideoController(IFileUrlService fileUrlService)
+        private readonly ApplicationDbContext _context;
+        public VideoController(IFileUrlService fileUrlService,ApplicationDbContext context)
         {
             _fileUrlService = fileUrlService;
+            _context = context;
             //模拟的数据库中存储的数据格式
-            _videos = new List<Video>
-        {
-            new Video
-            {
-                VideoId = 1,
-                Title = "Video Title 1",
-                Description = "Video Description 1",
-                PostedOn = DateTime.Now.AddDays(-10),
-                Duration = TimeSpan.FromMinutes(125),
-                Poster = "User 1",
-                VideoFilePath = "1.mp4",
-                ThumbnailsPath = "1.png"
-            },
-            new Video
-            {
-                VideoId = 2,
-                Title = "Video Title 2",
-                Description = "Video Description 2",
-                PostedOn = DateTime.Now.AddDays(-5),
-                Duration = TimeSpan.FromMinutes(98),
-                Poster = "User 2",
-                VideoFilePath = "2.mp4",
-                ThumbnailsPath = "2.png"
-            },
-        };
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Video>> GetAllVideos()
         {
-
-            foreach (var video in _videos)
+            // 从数据库获取视频信息
+            var videos = _context.Videos.ToList();
+            foreach (var video in videos)
             {
                 //调用服务拼接完整的url
                 video.ThumbnailsPath = _fileUrlService.GetFullUrl(video.ThumbnailsPath);
                 video.VideoFilePath = _fileUrlService.GetFullUrl(video.VideoFilePath);
             }
 
-            return Ok(_videos);
+            return Ok(videos);
         }
 
         [HttpGet("{id}")]
         public ActionResult<Video> GetVideoById(int id)
         {
-            var video = _videos.FirstOrDefault(v => v.VideoId == id);
+            // 从数据库中查找匹配ID的视频信息
+            var video = _context.Videos.FirstOrDefault(v => v.VideoId == id);
 
             if (video == null)
             {
+                // 如果没有找到视频，返回 NotFound
                 return NotFound();
             }
 
-            return video;
+            // 如果找到视频，调用服务拼接完整的url
+            video.ThumbnailsPath = _fileUrlService.GetFullUrl(video.ThumbnailsPath);
+            video.VideoFilePath = _fileUrlService.GetFullUrl(video.VideoFilePath);
+
+            // 返回找到的视频
+            return Ok(video);
         }
 
         // Later you can add more methods to handle POST, PUT, DELETE...
