e2ef98e3b3afc52ab7f7510cb659673c69af09c9
Dawn_Chen <1041031300@qq.com>
2024-03-18 15:31:41 +0800

    Beautified the login and registration interface

--- WebsiteUI/src/app/register/register.component.ts
+++ WebsiteUI/src/app/register/register.component.ts
@@ -1,31 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  user = {
-    username: '',
-    password: '',
-    email: ''
-  };
+  registerForm: FormGroup = new FormGroup({
+    'username': new FormControl(null, Validators.required),
+    'password': new FormControl(null, Validators.required),
+    'email': new FormControl(null, [
+      Validators.required,
+      Validators.email,
+    ]),
+  });
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private http: HttpClient,
+    private snackBar: MatSnackBar) {}
 
-  onRegister() {
-    this.http.post('/api/user/register', this.user)
-    .subscribe({
-      next: (response) => {
-        console.log('注册成功',response);
-      },
-      error: (error) => {
-        console.error('注册失败', error);
+
+    onSubmit() {
+      if (this.registerForm.invalid) {
+        this.snackBar.open('Please check your input!', 'Close', {
+          duration: 2000,
+        });
+        return;
       }
-    });
-  }
+  
+      const val = this.registerForm.value;
+  
+      this.userService.register(val.username, val.password, val.email)
+        .subscribe({
+          next: () => {
+            this.router.navigateByUrl('/login');
+            this.snackBar.open('Register successful, please log in!', 'Close', {
+              duration: 3000
+            });
+          },
+          error: (error) => {
+            if (error.error instanceof ErrorEvent) {
+              this.snackBar.open('Register failed, please check your network!', 'Close', {
+                duration: 2000,
+              });
+            } else {
+              this.snackBar.open('Register failed, please try again!', 'Close', {
+                duration: 2000,
+              });
+            }
+          }
+        });
+    }
 }
--- WebsiteUI/src/app/services/user.service.ts
+++ WebsiteUI/src/app/services/user.service.ts
@@ -22,6 +22,17 @@ export class UserService {
         throw error;
       })
     );
-    
+  }
+
+  register(username: string, password: string, email: string) {
+    return this.http.post<any>(`${environment.apiUrl}/User/register`, {"username":username, "password":password, "email":email})
+    .pipe(
+      catchError((error) => {
+        this.snackBar.open('Register failed!', 'Close', {
+          duration: 3000
+        });
+        throw error;
+      })
+    );
   }
 }
